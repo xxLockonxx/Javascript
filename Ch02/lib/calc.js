e547cdf05a0a50bc65c4d85e9d13b8b89789695b
xxLockonxx <ptsgrd@naver.com>
2020-06-08 16:18:26 +0900

    Ch02

---
+++ Ch02/lib/calc.js
@@ -0,0 +1,16 @@
+function plus(x, y){
+    var z = x + y;
+    return z;
+}
+function minus(x, y){
+    var z = x - y;
+    return z;
+}
+function multi(x, y){
+    var z = x * y;
+    return z;
+}
+function div(x, y){
+    var z = x / y;
+    return z;
+}
